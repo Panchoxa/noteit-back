afcb0dc8296d79e8c475896f08a77b655b37ca75
zaiats <azdronik@gmail.com>
2019-11-10 13:15:19 +0200

    created repositories

--- src/main/java/com/cratorschool/noteit/dao/NoteDAO.java
+++ src/main/java/com/cratorschool/noteit/dao/NoteDAO.java
@@ -1,11 +1,9 @@
 package com.cratorschool.noteit.dao;
-
 import com.cratorschool.noteit.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
 @Repository
 public interface NoteDAO extends JpaRepository<Note, UUID> {
 }
--- src/main/java/com/cratorschool/noteit/dao/NotebookDAO.java
+++ src/main/java/com/cratorschool/noteit/dao/NotebookDAO.java
@@ -1,9 +1,7 @@
 package com.cratorschool.noteit.dao;
-
 import com.cratorschool.noteit.model.Notebook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
-
 public interface NotebookDAO extends JpaRepository<Notebook, UUID> {
 }
