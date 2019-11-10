cee3776e8b8213c67f14a80504cb4642c20188df
zaiats <azdronik@gmail.com>
2019-11-10 13:10:59 +0200

    Created http file to test controllers

--- src/main/java/com/cratorschool/noteit/model/Notebook.java
+++ src/main/java/com/cratorschool/noteit/model/Notebook.java
@@ -1,23 +1,18 @@
 package com.cratorschool.noteit.model;
-
 import lombok.Getter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-
 @Entity
 @Getter
 public class Notebook {
-
     @Id
     @Column(columnDefinition = "BINARY(16)")
     @GeneratedValue
     private UUID id;
     private String name;
-
-
     // it includes many notes
     @OneToMany(fetch = FetchType.LAZY,mappedBy = "notebook", cascade = CascadeType.ALL)
     private List<Note> notes;
@@ -31,18 +26,15 @@ public class Notebook {
         this();
         this.name = name;
     }
-
     public Notebook(String id, String name){
         this();
         if(id != null){
             this.id = UUID.fromString(id);
         }
-
         Note note = new Note();
         Note note2 = new Note("title", "sdfgds", this);
         this.name = name;
     }
-
     public int getNBOfNotes() {
         return notes.size();
     }
