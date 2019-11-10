07d889504e1f40a73cbaa43590d95332c8be4466
zaiats <azdronik@gmail.com>
2019-11-10 13:14:53 +0200

    Created view models

--- src/main/java/com/cratorschool/noteit/api/viewmodel/NoteViewModel.java
+++ src/main/java/com/cratorschool/noteit/api/viewmodel/NoteViewModel.java
@@ -1,15 +1,11 @@
 package com.cratorschool.noteit.api.viewmodel;
-
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-
-
 @Getter
 @Setter
 public class NoteViewModel {
-
     private String id;
     private String title;
     private String text;
--- src/main/java/com/cratorschool/noteit/api/viewmodel/NotebookViewModel.java
+++ src/main/java/com/cratorschool/noteit/api/viewmodel/NotebookViewModel.java
@@ -1,13 +1,9 @@
 package com.cratorschool.noteit.api.viewmodel;
-
-
 import lombok.Getter;
 import lombok.Setter;
-
 @Getter
 @Setter
 public class NotebookViewModel {
-
     private String id;
     private String name;
     private int nbNotes;
