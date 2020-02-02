d6eb6be067c944e66170ecdf0d8d3122d48151cf
Max <azdronik@gmail.com>
2020-02-02 10:38:41 +0200

    work version

--- src/main/java/com/creatorschool/noteit/NoteitBackApplication.java
+++ src/main/java/com/creatorschool/noteit/NoteitBackApplication.java
@@ -2,10 +2,11 @@ package com.creatorschool.noteit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication(scanBasePackages={"com.creatorschool.noteit"})
-public class NoteitBackApplication {
+public class NoteitBackApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(NoteitBackApplication.class, args);
